3e1b095919552b0b1d03066fbd2d947ef98a5fd0
jdglaser <jarred.glaser@gmail.com>
2023-09-28 10:00:15 -0500

    Update resume and bullet points

--- src/static/jobs.ts
+++ src/static/jobs.ts
@@ -10,37 +10,30 @@ export const jobs: ExperienceItem[] = [
   {
     id: 1,
     companyName: "Northwestern Mutual",
-    title: "Senior Data Engineer",
-    time: "July 2023 - Present",
+    title: "Senior Software Engineer",
+    time: "April 2022 - Present",
     bulletPoints: [
-      "Built and maintained Gitlab CI/CD tooling using Python and Docker for our Snowflake and DBT-based data platform enabling self service access for engineering teams and leading to improved platform adoption across the department",
-      "Developed a Python FastAPI and React web application hosted on AWS EKS that served a custom Office web add-in allowing business users to publish custom Excel functions and driving improved accessibility for data consumers",
-      "Led meetings with engineering and business teams to consult on data engineering best practices and gather requirements for the data platform using agile best practices to prioritize incoming requests",
-      "Conducted daily agile standups leading agile ceremonies and working with the engineering team to delegate tasks"
+      "Architected and deployed a Java Spring and React web app with AWS EKS allowing investment analysts to manage and explore investment opportunities resulting in an increase of over $1.2m in investment profits for the company",
+      "Built and maintained GitLab CI/CD tooling using Python and Docker, facilitating self-service access to a Snowflake and DBT-based data platform and increasing its adoption across engineering teams",
+      "Developed a Python FastAPI and React web application hosted on AWS EKS that served an Office web add-in, leading to improved accessibility for data consumers",
+      "Built and deployed a custom Snowflake external function using AWS Lambda and API gateway, allowing analysts to enrich data by pulling information from internal APIs and services directly through Snowflake SQL queries",
+      "Developed a custom GitLab CI/CD template using Python and Docker to enhance config-as-code state management for our enterprise batch solution, improving developer productivity and reducing bugs when deploying batch pipelines",
+      "Led daily standups following agile best practices and allowing our team to efficiently handle incoming requests"
     ]
   },
   {
     id: 2,
     companyName: "Northwestern Mutual",
-    title: "Sr. Software Engineer",
-    time: "April 2022 - July 2023",
+    title: "Software Engineer",
+    time: "Jan 2020 - April 2022",
     bulletPoints: [
-      "Architected and deployed a Java/Spring and React web app with AWS EKS allowing investment analysts to manage and explore investment opportunities resulting in an increase of over $1.2m in investment profits for the company",
-      "Developed a custom data ingestion microservice using Java/Spring, AWS EKS, and S3 that integrated with a Snowflake data platform enabling our engineering team to easily onboard over 100GB of data from various sources",
-      "Modernized several legacy ETL processes to Python Docker batch jobs resulting in a ~80% decrease in data quality issues and unexpected failures and downtime for our daily batch processes",
+      "Developed a custom data ingestion microservice using Java/Spring, AWS EKS, and S3, facilitating the seamless onboarding of over 100GB of data into Snowflake",
+      "Modernized several legacy ETL pipelines using Python and Docker resulting in an ~80% decrease in data quality issues and unexpected failures and downtime for our daily batch processes",
+      "Developed and maintained a Java Spring and React web application on AWS (EKS, RDS, S3) enabling investment analysts to monitor and manage their investment portfolios resulting in increased investment revenue for the company"
     ]
   },
   {
     id: 3,
-    companyName: "Northwestern Mutual",
-    title: "Software Engineer",
-    time: "Jan 2020 - April 2022",
-    bulletPoints: [
-      "Developed and maintained a Java/Spring and React web application on AWS (EKS, RDS, S3) enabling investment analysts to monitor and manage their investment portfolios resulting in increased investment revenue for the company",
-    ]
-  },
-  {
-    id: 4,
     companyName: "Continuus Technologies",
     title: "Business Intelligence Engineer",
     time: "Sep 2019 - Jan 2020",
@@ -49,7 +42,7 @@ export const jobs: ExperienceItem[] = [
     ]
   },
   {
-    id: 5,
+    id: 4,
     companyName: "Uline",
     title: "Data Analyst",
     time: "Jan 2019 - Sep 2019",
@@ -58,7 +51,7 @@ export const jobs: ExperienceItem[] = [
     ]
   },
   {
-    id: 6,
+    id: 5,
     companyName: "Johnson Controls",
     title: "Business Intelligence Analyst",
     time: "Oct 2017 - Jan 2019",
